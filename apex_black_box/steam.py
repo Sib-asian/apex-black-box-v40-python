cbdc9e9852444ee7e81c4afa63ba65283783f903
Sib-asian <francescoprova67@gmail.com>
2026-03-17 14:59:25 +0100

    Add SteamAnalyzer class for quote movement detection and pre-match advice generation

---
+++ apex_black_box/steam.py
@@ -0,0 +1,11 @@
+class SteamAnalyzer:
+    def __init__(self, quotes):
+        self.quotes = quotes
+
+    def detect_movement(self):
+        # Logic for detecting quote movements
+        pass
+
+    def generate_advice(self):
+        # Logic for generating pre-match advice
+        pass
