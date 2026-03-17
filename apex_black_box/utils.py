06df28908d758783f6103c2475a58042c7c4c32f
Sib-asian <francescoprova67@gmail.com>
2026-03-17 15:05:31 +0100

    Create utils.py with utility functions

---
+++ apex_black_box/utils.py
@@ -0,0 +1,28 @@
+def format_odd(odd):
+    # Implementation of formatting odd
+    pass
+
+
+def format_probability(probability):
+    # Implementation of formatting probability
+    pass
+
+
+def calculate_edge(probability, odd):
+    # Implementation for calculating edge
+    pass
+
+
+def confidence_interval(data):
+    # Implementation for calculating confidence interval
+    pass
+
+
+def poisson_ci(count, confidence_level=0.95):
+    # Implementation for Poisson confidence interval
+    pass
+
+
+def parse_result(result):
+    # Implementation for result parsing
+    pass
